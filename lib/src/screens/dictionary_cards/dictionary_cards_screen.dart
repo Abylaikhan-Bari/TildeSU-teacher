8c9aee009c7430d34913ad30b845cfa1d9125f4a
Abylaikhan Bariyev <abylaikhanbariev7@gmail.com>
2024-03-27 22:36:23 +0500

    Adding new exercise types

--- lib/src/screens/dictionary_cards/dictionary_cards_screen.dart
+++ lib/src/screens/dictionary_cards/dictionary_cards_screen.dart
@@ -8,307 +8,213 @@ class DictionaryCardsScreen extends StatefulWidget {
 
 class _DictionaryCardsScreenState extends State<DictionaryCardsScreen> {
   final _formKey = GlobalKey<FormState>();
-  final _statementController = TextEditingController();
-  bool _isTrue = true; // Default to true
+  final _englishWordController = TextEditingController();
+  final _kazakhWordController = TextEditingController();
+  final _russianWordController = TextEditingController();
   String _selectedLevel = 'A1'; // Default selected level
 
   @override
   void dispose() {
-    _statementController.dispose();
+    _englishWordController.dispose();
+    _kazakhWordController.dispose();
+    _russianWordController.dispose();
     super.dispose();
   }
 
-  void _addTrueOrFalseExercise() async {
+  void _addOrUpdateDictionaryCard({String? cardId}) async {
     if (_formKey.currentState!.validate()) {
-      final exerciseData = {
-        'statement': _statementController.text.trim(),
-        'isTrue': _isTrue,
+      final cardData = {
+        'wordEnglish': _englishWordController.text.trim(),
+        'wordKazakh': _kazakhWordController.text.trim(),
+        'wordRussian': _russianWordController.text.trim(),
       };
 
-      try {
-        // Query the Firestore collection to get existing exercise IDs
-        final existingExercises = await FirebaseFirestore.instance
-            .collection('levels')
-            .doc(_selectedLevel)
-            .collection('trueOrFalse')
-            .get();
+      final collectionReference = FirebaseFirestore.instance
+          .collection('levels')
+          .doc(_selectedLevel)
+          .collection('dictionaryCards');
 
-        // Extract existing IDs
-        final existingIds = existingExercises.docs.map((exercise) => exercise.id);
-
-        // Find the next available ID
-        int nextId = 1;
-        while (existingIds.contains('statementId$nextId')) {
-          nextId++;
-        }
-        final newExerciseId = 'statementId$nextId';
-
-        // Add the exercise with the new ID
-        await FirebaseFirestore.instance
-            .collection('levels')
-            .doc(_selectedLevel)
-            .collection('trueOrFalse')
-            .doc(newExerciseId)
-            .set(exerciseData);
-        _clearForm();
-      } catch (error) {
-        // Handle errors here
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text(_translate('Failed to add true or false exercise: $error'))),
-        );
+      if (cardId == null) {
+        // Adding a new card
+        await collectionReference.add(cardData);
+      } else {
+        // Updating an existing card
+        await collectionReference.doc(cardId).update(cardData);
       }
+
+      _clearForm();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Dictionary card ${cardId == null ? "added" : "updated"} successfully')),
+      );
     }
   }
 
   void _clearForm() {
-    _statementController.clear();
-    setState(() {
-      _isTrue = true;
-    });
-  }
-  Future<void> _updateTrueOrFalseExercise(String exerciseId) async {
-    if (_formKey.currentState!.validate()) {
-      final exerciseData = {
-        'statement': _statementController.text.trim(),
-        'isTrue': _isTrue,
-      };
-
-      try {
-        await FirebaseFirestore.instance
-            .collection('levels')
-            .doc(_selectedLevel)
-            .collection('trueOrFalse')
-            .doc(exerciseId)
-            .update(exerciseData);
-        _clearForm();
-      } catch (error) {
-        // Handle errors here
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Failed to update true or false exercise: $error')),
-        );
-      }
-    }
+    _englishWordController.clear();
+    _kazakhWordController.clear();
+    _russianWordController.clear();
   }
 
-  Future<void> _deleteTrueOrFalseExercise(String exerciseId) async {
-    await showDialog(
+  Future<void> _deleteDictionaryCard(String cardId) async {
+    final shouldDelete = await showDialog<bool>(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Delete True/False Exercise'),
-          content: Text('Are you sure you want to delete this exercise?'),
+          title: Text('Delete Dictionary Card'),
+          content: Text('Are you sure you want to delete this card?'),
           actions: <Widget>[
             TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
+              onPressed: () => Navigator.of(context).pop(false),
               child: Text('Cancel'),
             ),
             TextButton(
-              onPressed: () async {
-                try {
-                  await FirebaseFirestore.instance
-                      .collection('levels')
-                      .doc(_selectedLevel)
-                      .collection('trueOrFalse')
-                      .doc(exerciseId)
-                      .delete();
-                  Navigator.of(context).pop();
-                } catch (error) {
-                  // Handle error
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Failed to delete exercise: $error')),
-                  );
-                }
-              },
-              child: Text('Yes'),
+              onPressed: () => Navigator.of(context).pop(true),
+              child: Text('Delete'),
             ),
           ],
         );
       },
     );
+
+    if (shouldDelete == true) {
+      await FirebaseFirestore.instance
+          .collection('levels')
+          .doc(_selectedLevel)
+          .collection('dictionaryCards')
+          .doc(cardId)
+          .delete();
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Dictionary card deleted successfully')),
+      );
+    }
   }
 
-  // Remaining code for updating and deleting exercises remains the same
+  Widget _buildForm({required bool isUpdating, String? cardId}) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextFormField(
+            controller: _englishWordController,
+            decoration: InputDecoration(labelText: 'English Word'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter the English word';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: _kazakhWordController,
+            decoration: InputDecoration(labelText: 'Kazakh Word'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter the Kazakh word';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: _russianWordController,
+            decoration: InputDecoration(labelText: 'Russian Word'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter the Russian word';
+              }
+              return null;
+            },
+          ),
+          TextButton(
+            onPressed: () => _addOrUpdateDictionaryCard(cardId: isUpdating ? cardId : null),
+            child: Text(isUpdating ? 'Update' : 'Add'),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(_translate('Dictionary cards for Level $_selectedLevel')),
-        backgroundColor: Color(0xFF34559C),
+        appBar: AppBar(
+        title: Text('Dictionary Cards for Level $_selectedLevel'),
+    ),
+    body: Column(
+    children: [
+    DropdownButton<String>(
+    value: _selectedLevel,
+    onChanged: (String? newValue) {
+    if (newValue != null) {
+    setState(() {
+    _selectedLevel = newValue;
+    });
+    }
+    },
+    items: <String>['A1', 'A2', 'B1', 'B2', 'C1', 'C2']
+        .map<DropdownMenuItem<String>>((String value) {
+      return DropdownMenuItem<String>(
+        value: value,
+        child: Text(value),
+      );
+    }).toList(),
+    ),
+      Expanded(
+        child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('levels')
+              .doc(_selectedLevel)
+              .collection('dictionaryCards')
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+              return Center(child: Text('No dictionary cards found'));
+            }
+            return ListView(
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                Map<String, dynamic> card = document.data() as Map<String, dynamic>;
+                return ListTile(
+                  title: Text(card['wordEnglish'] ?? ''),
+                  subtitle: Text('Kazakh: ${card['wordKazakh']} | Russian: ${card['wordRussian']}'),
+                  onTap: () {
+                    _englishWordController.text = card['wordEnglish'];
+                    _kazakhWordController.text = card['wordKazakh'];
+                    _russianWordController.text = card['wordRussian'];
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('Update Dictionary Card'),
+                          content: _buildForm(isUpdating: true, cardId: document.id),
+                        );
+                      },
+                    );
+                  },
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () => _deleteDictionaryCard(document.id),
+                  ),
+                );
+              }).toList(),
+            );
+          },
+        ),
       ),
-      body: Column(
-        children: [
-          DropdownButton<String>(
-            value: _selectedLevel,
-            onChanged: (String? newValue) {
-              if (newValue != null) {
-                setState(() {
-                  _selectedLevel = newValue;
-                });
-              }
-            },
-            items: <String>['A1', 'A2', 'B1', 'B2', 'C1', 'C2'] // Add more levels if needed
-                .map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-          ),
-          Expanded(
-            child: StreamBuilder<QuerySnapshot>(
-              stream: FirebaseFirestore.instance
-                  .collection('levels')
-                  .doc(_selectedLevel)
-                  .collection('trueOrFalse')
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                } else {
-                  final exercises = snapshot.data!.docs;
-                  return ListView.builder(
-                    itemCount: exercises.length,
-                    itemBuilder: (context, index) {
-                      final exercise = exercises[index];
-                      return Card(
-                        child: ListTile(
-                          title: Text(_translate('Exercise ${exercise.id}: ${exercise['statement']}')),
-                          subtitle: Text(_translate('Statement: ${exercise['statement']}')),
-                          onTap: () {
-                            // Remaining code for updating and deleting exercises remains the same
-                            // Show dialog to update true/false exercise
-                            _statementController.text = exercise['statement'];
-                            _isTrue = exercise['isTrue'];
-                            showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  title: Text(_translate('Update True/False Exercise')),
-                                  content: Form(
-                                    key: _formKey,
-                                    child: Column(
-                                      mainAxisSize: MainAxisSize.min,
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        TextFormField(
-                                          controller: _statementController,
-                                          decoration: InputDecoration(labelText: _translate('Statement')),
-                                          validator: (value) {
-                                            if (value == null || value.isEmpty) {
-                                              return _translate('Please enter a statement');
-                                            }
-                                            return null;
-                                          },
-                                        ),
-                                        Row(
-                                          children: [
-                                            Text(_translate('Is True: ')),
-                                            Checkbox(
-                                              value: _isTrue,
-                                              onChanged: (value) {
-                                                setState(() {
-                                                  _isTrue = value!;
-                                                });
-                                              },
-                                            ),
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  actions: [
-                                    TextButton(
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: Text(_translate('Cancel')),
-                                    ),
-                                    TextButton(
-                                      onPressed: () {
-                                        _updateTrueOrFalseExercise(exercise.id);
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: Text(_translate('Update')),
-                                    ),
-                                  ],
-                                );
-                              },
-                            );
-                          },
-                          trailing: IconButton(
-                            icon: Icon(Icons.delete),
-                            onPressed: () {
-                              // Remaining code for updating and deleting exercises remains the same
-                              _deleteTrueOrFalseExercise(exercise.id);
-                            },
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                }
-              },
-            ),
-          ),
-        ],
-      ),
+    ],
+    ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          _clearForm();
           showDialog(
             context: context,
             builder: (BuildContext context) {
               return AlertDialog(
-                title: Text(_translate('Add True/False Exercise')),
-                content: Form(
-                  key: _formKey,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TextFormField(
-                        controller: _statementController,
-                        decoration: InputDecoration(labelText: _translate('Statement')),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return _translate('Please enter a statement');
-                          }
-                          return null;
-                        },
-                      ),
-                      Row(
-                        children: [
-                          Text(_translate('Is True: ')),
-                          Checkbox(
-                            value: _isTrue,
-                            onChanged: (value) {
-                              setState(() {
-                                _isTrue = value!;
-                              });
-                            },
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                actions: [
-                  TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Text(_translate('Cancel')),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      _addTrueOrFalseExercise();
-                      Navigator.of(context).pop();
-                    },
-                    child: Text(_translate('Add')),
-                  ),
-                ],
+                title: Text('Add Dictionary Card'),
+                content: _buildForm(isUpdating: false),
               );
             },
           );
@@ -317,10 +223,5 @@ class _DictionaryCardsScreenState extends State<DictionaryCardsScreen> {
       ),
     );
   }
-
-  String _translate(String text) {
-    // Here you can implement your translation logic
-    // For now, let's just return the input text
-    return text;
-  }
 }
+
